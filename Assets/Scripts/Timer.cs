e0ff00a51cbcd5e9088959340645f6b42edd7273
TimeTraverler <1242857339@qq.com>
2022-09-21 23:34:06 +0800

    Update project

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public Material material;
+
+    IEnumerator Clock()
+    {
+        yield return new WaitForSeconds(7f);
+        GetComponent<ShallowWater>().enabled = true;
+        yield return new WaitForSeconds(3f);
+        ShallowWater.watercondition = 2;
+    }
+
+    void Start()
+    {
+        StartCoroutine("Clock");
+    }
+}
